c1d2ccdb88282756d206540451eb1fe4cd5c8c29
Yosmany Garcia <yosmanyga@gmail.com>
2014-05-27 23:43:54 -0400

    Added "push" and "approach" methods

--- src/Cubalider/Component/Sms/Manager/BulkManagerInterface.php
+++ src/Cubalider/Component/Sms/Manager/BulkManagerInterface.php
@@ -8,6 +8,23 @@ namespace Cubalider\Component\Sms\Manager;
 interface BulkManagerInterface
 {
     /**
+     * Adds given bulk into the last position.
+     *
+     * @internal
+     * @param mixed $bulk
+     * @return void
+     */
+    public function push($bulk);
+
+    /**
+     * Gets the bulk at first position.
+     *
+     * @internal
+     * @return mixed
+     */
+    public function approach();
+    
+    /**
      * Removes the bulk at first position.
      *
      * @internal
